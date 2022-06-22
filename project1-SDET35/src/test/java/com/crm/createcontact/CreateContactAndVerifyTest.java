7a1bc5c2a01434c48f3f87e0c3d00242e4bb3f7e
FARHAN SD <FARHAN SD@LAPTOP-FN6RJ6BB>
2022-06-22 15:39:23 +0530

    nn

--- project1-SDET35/src/test/java/com/crm/createcontact/CreateContactAndVerifyTest.java
+++ project1-SDET35/src/test/java/com/crm/createcontact/CreateContactAndVerifyTest.java
@@ -13,7 +13,7 @@ public class CreateContactAndVerifyTest extends BaseClass{
 	@Test(groups={"smokeTest"})
 	public void createContactAndVerifyTest() throws Throwable {
 
-		//hello Master
+		//hello brother
 		
 		//generate random number
 		int randnum = jUtil.getRandomNumber();
